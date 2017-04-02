75ee9ddffa3fb0751a1a73b2543f0ef4eb002bca
VICKY\vickssarv <vickssarv1996@gmail.com>
2017-04-02 11:20:59 +0530

    Gradle build issue fix after Development code merge - Sandy

--- app/src/main/java/com/example/vickssarv/homescreenf/MapsActivity.java
+++ app/src/main/java/com/example/vickssarv/homescreenf/MapsActivity.java
@@ -23,12 +23,6 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
-    private GoogleMap mMap;
-
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
@@ -138,29 +132,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
+//    /**
+//     * Manipulates the map once available.
+//     * This callback is triggered when the map is ready to be used.
+//     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+//     * we just add a marker near Sydney, Australia.
+//     * If Google Play services is not installed on the device, the user will be prompted to install
+//     * it inside the SupportMapFragment. This method will only be triggered once the user has
+//     * installed Google Play services and returned to the app.
+//     */
+//    @Override
+//    public void onMapReady(GoogleMap googleMap) {
+//        mMap = googleMap;
+//        Double lat= (Double) getIntent().getExtras().get("lat");
+//        Double lon= (Double) getIntent().getExtras().get("lon");
+//        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(lat,lon);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Alert"));
+//
+//        int zoomLevel = 16; //This goes up to 21
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, zoomLevel));
+//    }
 
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        Double lat= (Double) getIntent().getExtras().get("lat");
-        Double lon= (Double) getIntent().getExtras().get("lon");
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(lat,lon);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Alert"));
-
-        int zoomLevel = 16; //This goes up to 21
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, zoomLevel));
-    }
-}
 
     public void onRequestPermissionsResult(int requestCode,
                                            String permissions[], int[] grantResults) {
